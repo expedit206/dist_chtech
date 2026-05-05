2137727f57ad790bbf6e191fe617acf392787c83
Expedit206 <mciagnessi@gmail.com>
2026-05-05 15:49:24 +0100

    review

---
+++ assets/check-DBQWb1XC.js
@@ -0,0 +1,6 @@
+import{a0 as c}from"./index-DZUsi3BC.js";/**
+ * @license lucide-vue-next v0.575.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=c("check",[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]]);export{a as C};
